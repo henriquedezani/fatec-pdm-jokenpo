0bff5b5f9464a30ffc81fe39bd153c4112a87b6f
Henrique Dezani <dezani@gmail.com>
2021-08-26 18:00:36 -0300

    Finaliza o jogo

--- lib/telaDoJogo.dart
+++ lib/telaDoJogo.dart
@@ -14,14 +14,44 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
   String? itemSelecionado;
   String? computadorSelecionou;
 
-  void _onItemClick(String item) {
+  void _onItemClick(String item, BuildContext context) {
     final random = Random();
 
     var opcoes = ["pedra", "papel", "tesoura"];
 
+    String? ganhou;
+
+    computadorSelecionou = opcoes[random.nextInt(3)];
+
+    if (item == 'pedra' && computadorSelecionou == 'papel')
+      ganhou = 'computador';
+    if (item == 'papel' && computadorSelecionou == 'tesoura')
+      ganhou = 'computador';
+    if (item == 'tesoura' && computadorSelecionou == 'pedra')
+      ganhou = 'computador';
+    if (item == 'papel' && computadorSelecionou == 'pedra') ganhou = 'jogador';
+    if (item == 'tesoura' && computadorSelecionou == 'papel')
+      ganhou = 'jogador';
+    if (item == 'pedra' && computadorSelecionou == 'tesoura')
+      ganhou = 'jogador';
+    if (item == computadorSelecionou) ganhou = 'empate';
+
+    var alert = AlertDialog(
+      title: Text("Vencedor"),
+      content: Text(ganhou!),
+      actions: [
+        ElevatedButton(
+          child: Text("Ok"),
+          onPressed: () => Navigator.of(context).pop(),
+        )
+      ],
+    );
+
+    showDialog(
+        context: context, builder: (_) => alert, barrierDismissible: false);
+
     setState(() {
       itemSelecionado = item;
-      computadorSelecionou = opcoes[random.nextInt(3)];
     });
   }
 
@@ -64,7 +94,7 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
                     Icons.favorite,
                     selected: itemSelecionado == "pedra",
                   ),
-                  onTap: () => _onItemClick("pedra"),
+                  onTap: () => _onItemClick("pedra", context),
                 ),
                 GestureDetector(
                   child: Item(
@@ -72,7 +102,7 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
                     Icons.battery_alert,
                     selected: itemSelecionado == "papel",
                   ),
-                  onTap: () => _onItemClick("papel"),
+                  onTap: () => _onItemClick("papel", context),
                 ),
                 GestureDetector(
                   child: Item(
@@ -80,7 +110,7 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
                     Icons.wifi,
                     selected: itemSelecionado == "tesoura",
                   ),
-                  onTap: () => _onItemClick("tesoura"),
+                  onTap: () => _onItemClick("tesoura", context),
                 ),
               ],
             ),
